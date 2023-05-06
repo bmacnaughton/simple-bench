7d5ab6c2afb7c3e79315113560b35668f01411bc
bmacnaughton <bmacnaughton@gmail.com>
2023-05-06 07:04:27 -0700

    eslint cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,7 +66,7 @@ module.exports = {
     "no-undef": 2,
     //"no-unused-vars": [2, {"args": "after-used"}],
     // don't complain about unused args
-    "no-unused-vars": [2, {"args": "none"}],
+    "no-unused-vars": [2, { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
 
     // Node.js and CommonJS
     // http://eslint.org/docs/rules/#nodejs-and-commonjs
